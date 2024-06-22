47b851b178b153a5687b2e440a84b4530057bf9e
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-06-22 19:29:02 +0200

    tescikowe skrypty z mojego projektu (NaVv666)

---
+++ Assets/Scripts/NaVv666 tescikowe/FireBullet.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    private float speed = 10f;
+    private Animator animator;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(disableBullet(5f));
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    public float Speed { get { return speed; } set { speed = value; } }
+
+    private void Move()
+    {
+        Vector3 temp = transform.position;
+        temp.x += speed * Time.deltaTime;
+        transform.position = temp;
+    }
+
+    IEnumerator disableBullet(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == Tags.SNAIL_TAG || collision.gameObject.tag == Tags.BEETLE_TAG || collision.gameObject.tag == Tags.BIRD_TAG || collision.gameObject.tag == Tags.SPIDER_TAG)
+        {
+            speed = 0;
+            animator.Play("PlayerBullet");
+            StartCoroutine(disableBullet(0.25f));
+        }
+    }
+}
---
+++ Assets/Scripts/NaVv666 tescikowe/SpiderScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderScript : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D spiderBody;
+
+    private Vector3 moveDirection = Vector3.down;
+    private Coroutine spiderMovement;
+
+    public float moveDistance = 0.5f;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        spiderBody = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        spiderMovement = StartCoroutine(ChangeMovement(0.5f));
+    }
+
+    void Update()
+    {
+        MoveSpider();
+    }
+
+    void MoveSpider()
+    {
+        transform.Translate(moveDirection * Time.deltaTime * 10);
+    }
+    IEnumerator ChangeMovement(float moveDistance)
+    {
+        yield return new WaitForSeconds(moveDistance);
+
+        if(moveDirection == Vector3.down)
+        {
+            moveDirection = Vector3.up;
+        } else
+        {
+            moveDirection = Vector3.down;
+        }
+        spiderMovement = StartCoroutine(ChangeMovement(0.5f));
+    }
+
+    IEnumerator SpiderDead()
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == Tags.BULLET_TAG)
+        {
+            animator.Play("SpiderDead");
+            moveDirection = Vector3.down;
+            spiderBody.bodyType = RigidbodyType2D.Dynamic;
+            StartCoroutine(SpiderDead());
+            StopCoroutine(spiderMovement);
+        }
+    }
+}
